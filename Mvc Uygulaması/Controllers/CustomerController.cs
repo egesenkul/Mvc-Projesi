da12ee19f3efb92649c7253f1a2300ac6e5a2bc3
Ege Şenkul <egesenkul35@hotmail.com>
2019-12-12 16:47:28 +0300

    Müşteri bilgilerini görme ve güncelleme işlemi tamamlandı

--- Mvc Uygulaması/Controllers/CustomerController.cs
+++ Mvc Uygulaması/Controllers/CustomerController.cs
@@ -16,5 +16,41 @@ namespace Mvc_Uygulaması.Controllers
             List<Customers> customers = dbModel.Customers.ToList();
             return View(customers);
         }
+
+        public ActionResult AddNew()
+        {
+            return RedirectToAction("Detail", new { userID = -1, isUpdate = false });
+        }
+
+        public ActionResult Detail(string userID,bool isUpdate)
+        {
+            Customers customer = dbModel.Customers.FirstOrDefault(p => p.CustomerID.Equals(userID));
+            if (customer != null)
+            {
+                if(isUpdate)
+                    ViewBag.Message = "Kayıt Güncellendi";
+                return View(customer);
+            }
+            else
+                return View();
+        }
+
+        public ActionResult Update(Customers willUpdated, bool isUpdate)
+        {
+            if (!string.IsNullOrEmpty(willUpdated.CustomerID))
+            {
+                var updatedEntity = dbModel.Entry(willUpdated);
+                updatedEntity.State = System.Data.Entity.EntityState.Modified;
+                dbModel.SaveChanges();
+                return RedirectToAction("Detail", new { userID = willUpdated.CustomerID, isUpdate = true });
+            }
+            else
+            {
+                var added = dbModel.Entry(willUpdated);
+                added.State = System.Data.Entity.EntityState.Added;
+                dbModel.SaveChanges();
+                return RedirectToAction("Detail", new { userID = added.Entity.CustomerID, isUpdate = false });
+            }
+        }
     }
 }
